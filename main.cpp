aa592d5690e9aec9236e2a5d1a81f13aba8a3110
Hannes Jonsson <hanjon98@gmail.com>
2022-12-03 17:16:12 +0100

    Day 2

--- main.cpp
+++ main.cpp
@@ -18,20 +18,35 @@ void day1(cstr in)
 
 void day2(cstr in)
 {
-    
+    auto s = split(in, "\n"_s);
+    int p = 0;
+    int p1 = 0;
+    for(umm i = 0; i < size(s); i++)
+    {
+        if(size(s[i]))
+        {
+            int a = s[i][0] - 'A';
+            int b = s[i][2] - 'X';
+            int r = wrap(b - a, -1, 1) + 1;
+            p += b + 1 + r * 3;
+            int c = wrap(b - 1 + a, 0, 2);
+            p1 += c + 1 + b * 3;
+        }
+    }
+    print("Total score: %\n", p);
+    print("Total score2: %\n", p1);
 }
 
 int main()
 {
     try
     {
-        dstr in = filestr("day1.txt"_s);
-        day1(in);
+        dstr in = filestr("day2.txt"_s);
+        day2(in);
     }
     catch(const error& e)
     {
         print("Unhandled error: %\n", e.what());
     }
-    
     return 0;
 }
